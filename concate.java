057ba5f891199b6a2c70a2e1f23eb55032cb0e87
manniiikaam <manickasaravanan76@gmail.com>
2025-03-12 15:51:34 +0530

    smart

---
+++ concate.java
@@ -0,0 +1,10 @@
+public class concate
+{
+    public static void main(String[]args)
+    {
+        String firstname="apple";
+        String lastname="mango";
+        String fullname=firstname+" "+lastname;
+        System.out.println("fullname:"+fullname);
+    }
+}
