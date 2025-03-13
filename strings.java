d1c87f2f66be4dbe25225e9e494eb70c9e2857fe
manniiikaam <manickasaravanan76@gmail.com>
2025-03-13 14:21:05 +0530

    length of the string'

---
+++ strings.java
@@ -0,0 +1,8 @@
+public class strings {
+    public static void main(String[] args)
+     {
+        String str = "apple"; 
+        int len = str.length(); 
+        System.out.println("The length of the string is: " + len); 
+    }
+}
