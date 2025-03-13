971d87376ea9d870dc17b617fb97783e1cb8bf61
manniiikaam <manickasaravanan76@gmail.com>
2025-03-13 12:11:27 +0530

    boolean true &&

---
+++ logical.java
@@ -0,0 +1,18 @@
+public class logical
+{
+    public static void main(String[]args)
+    {
+        int a=10;
+        int b=20;
+        if(a>b && a==b)
+        {
+            System.out.println("it is true");
+        }
+        else
+        {
+            System.out.println("it is false");
+        }
+    }
+}
+        
+
