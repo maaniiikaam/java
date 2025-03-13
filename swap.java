c2a1cb065b8587085d4ce6db4cd2f44271d78769
manniiikaam <manickasaravanan76@gmail.com>
2025-03-13 13:57:02 +0530

    with using third party

--- swap.java
+++ swap.java
@@ -4,14 +4,11 @@ public class swap
     {
         int a=5;
         int b=6;
-        System.out.println("before swapping");
+         a=a+b;
+         b=b-a;
+         a=a-b;
         System.out.println("a:"+a);
         System.out.println("b:"+b);
-        int temp=a;
-        a=b;
-        b=temp;
-        System.out.println("after swapping");
-        System.out.println("a:"+a);
-        System.out.println("b:"+b);
+
     }
 }
