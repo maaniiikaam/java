4c1ce8797c6806e0fa065612eb82d0218c875412
manniiikaam <manickasaravanan76@gmail.com>
2025-03-13 10:36:08 +0530

    doubletoint

---
+++ welcome2.java
@@ -0,0 +1,9 @@
+public class welcome2
+{
+    public static void main(String[]args)
+    {
+        double mydouble=10.5;
+        int mymark=(int)mydouble;
+        System.out.println(mymark);
+    }
+}
