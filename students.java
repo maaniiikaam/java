b301e780b98e21e1279a305663dbe067b8007900
manniiikaam <manickasaravanan76@gmail.com>
2025-03-13 09:52:45 +0530

    exampleproblem

---
+++ students.java
@@ -0,0 +1,14 @@
+public class students
+{
+    public static void main(String[]args)
+    {
+    String name="manickam";
+    String subjectname="java";
+    int mark=89;
+    char grade='A';
+    System.out.println(name);
+    System.out.println(subjectname);
+    System.out.println(mark);
+    System.out.println(grade);
+    }
+}
