2df84d0ccf28f1ef72eb369dba1e488b5af04163
manniiikaam <manickasaravanan76@gmail.com>
2025-03-13 11:46:02 +0530

    comparing string

---
+++ comparison.java
@@ -0,0 +1,17 @@
+public class comparison
+{
+    public static void main(String[]args)
+    {
+        String game1="cricket";
+        String game2="football";
+        if(game1==game2)
+        {
+            System.out.println("it is true");
+        }
+        else
+        {
+            System.out.println("it is false");
+         }
+        
+    }
+}
