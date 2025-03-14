b77d8840f51ce9441b7beafb081a06ce77ea0db2
manniiikaam <manickasaravanan76@gmail.com>
2025-03-14 14:15:45 +0530

    multidimensional array sum

---
+++ dimension.java
@@ -0,0 +1,8 @@
+public class dimension
+{
+    public static void main(String[]args)
+    {
+        String[][] game={{"cricket","ludo"},{"carrom","football"}};
+        System.out.println(game[1][1]);
+    }
+}
