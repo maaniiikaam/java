4c985dfd0852bd1e431f762c7bd977f839ba4a98
manniiikaam <manickasaravanan76@gmail.com>
2025-03-14 13:57:50 +0530

    array to find the length

--- arrays.java
+++ arrays.java
@@ -4,6 +4,9 @@ public class arrays
     {
     int[] a={12,23,45};
     String[] name={"manickam","jkrish","manik"};
+    System.out.println(name[0]);
+    System.out.println(name[1]);
     System.out.println(name[2]);
+    System.out.println("length of the array:"+name.length);
     }
 }
