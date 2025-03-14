bd6e89b65c59ec523f05d7e357d5049b23444c82
manniiikaam <manickasaravanan76@gmail.com>
2025-03-14 13:51:39 +0530

    array using index value

---
+++ arrays.java
@@ -0,0 +1,9 @@
+public class arrays
+{
+    public static void main(String[]args)
+    {
+    int[] a={12,23,45};
+    String[] name={"manickam","jkrish","manik"};
+    System.out.println(name[2]);
+    }
+}
