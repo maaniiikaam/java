f837a4f242f587ef3698058ef9860d75469a1ca8
manniiikaam <manickasaravanan76@gmail.com>
2025-03-14 13:20:44 +0530

    calling functionname ex:method sum

---
+++ method.java
@@ -0,0 +1,11 @@
+public class method
+{
+    static void mymethod()
+    {
+        System.out.println("welcome");
+    }
+    public static void main(String[] args)
+    {
+        mymethod();
+    }
+}
