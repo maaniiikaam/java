64514fdef700cd54b601c992c2be9614ad820375
manniiikaam <manickasaravanan76@gmail.com>
2025-03-14 10:07:11 +0530

    checking vowels

---
+++ vowels.java
@@ -0,0 +1,17 @@
+public class vowels
+{
+    public static void main(String[]args)
+    {
+        char ch='r';
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        {
+            System.out.println("it is a vowel");
+        }
+        else
+        {
+            System.out.println("it is not a vowel");
+
+        }
+        
+    }
+}
