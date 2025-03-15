062633ce77879d7dca5fa082372c31929932d12f
manniiikaam <manickasaravanan76@gmail.com>
2025-03-15 12:13:44 +0530

    finding username by scanner'

---
+++ name.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class name{
+    public static void main(String[]args)
+    {
+        Scanner myobj=new Scanner(System.in);
+        System.out.println("Enter username");
+        String username=myobj.next();
+        System.out.println("the username is"+username);
+    }
+}
